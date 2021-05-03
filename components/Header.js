b7a4b79c0fc8f8e7e59f4220a7f0863e1edf30bb
João Fernando Corsini <jf.corsini@gmail.com>
2021-05-03 16:56:45 +0300

    Remove non-needed headers

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,7 @@ import { headerBanner, headerTitle } from "../styles/header";
 export default function Header(props) {
   return (
     <>
-      <a href="https://fauna.com" target="_blank" rel="noopener noreferrer">
-        <img
-          className={headerBanner.className}
-          src="/static/fauna-logo-blue.png"
-          height="35px"
-          width="auto"
-        />
-      </a>
-      <h1 className={headerTitle.className}>Guestbook</h1>
+      <h1 className={headerTitle.className}>AviGuardX</h1>
       {headerBanner.styles}
       {headerTitle.styles}
     </>
