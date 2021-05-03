1d8c7823b31e987f6b36022dbf12c2441e1cc5a9
João Fernando Corsini <jf.corsini@gmail.com>
2021-05-03 18:17:30 +0300

    Add secret so only I can create entries

--- pages/api/entries.js
+++ pages/api/entries.js
@@ -2,11 +2,15 @@ import { createEntry } from "../../graphql/api";
 
 export default async function entriesHandler(req, res) {
   const { body, method } = req;
+  const { secret, ...entryData } = body;
+  if (secret !== process.env.SUPER_SECRET) {
+    return res.status(404).json({ error: "Wrong secret. Don't hack pls" });
+  }
 
   switch (method) {
     case "POST":
       // Get data from your database
-      const gqlResponse = await createEntry(body);
+      const gqlResponse = await createEntry(entryData);
       if (gqlResponse.errors) {
         return res.status(404).json(gqlResponse);
       }
