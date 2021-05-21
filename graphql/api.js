b1a53cbcf377ee54ef69bec20b47143fbf077bcf
João Fernando Corsini <jf.corsini@gmail.com>
2021-05-21 08:47:45 +0300

    Increase query limit just in case

--- graphql/api.js
+++ graphql/api.js
@@ -36,7 +36,7 @@ export const fetchEntries = async () => {
       after
     }
   }`;
-  const size = 500;
+  const size = 1000;
   const res = await fetch(process.env.NEXT_PUBLIC_FAUNADB_GRAPHQL_ENDPOINT, {
     method: "POST",
     headers: {
